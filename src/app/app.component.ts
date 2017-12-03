5b140a2cc294fb9b8152a291ea395efa2a8eb004
anything <anything@gmail.com>
2017-12-03 15:09:17 +0530

    event example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message = '<b>Thank you for your subscription:)</b>';
+  message = 'Thank you for your subscription:)';
   news = "Trending news";
-  name="Indresh Singh";
-  msgcls  = "red";
+  name = "Indresh Singh";
+  msgcls = "red";
+
+  classesSet = {
+    "bold":false,
+    "italic":false
+  };
+
+  classesArray = [
+    "yellow",
+    "bold",
+    "italic"
+
+  ];
 
   changeColor(cls) {
     this.msgcls = cls;
   }
+
+  showAlert() {
+    window.alert("Hi All");
+  }
+
+  showByName(name:string){
+    window.alert("Hi, "+ name);
+  }
+  captureEvent(event) {
+    console.log(event);
+  }
+
+  ctrlClick(event) {
+    if(event.ctrlKey){
+      window.alert("Showing only if ctrl key pressed: "+event.ctrlKey);
+    }
+  }
+
+
+
+
+
+
+
+
+
 }
